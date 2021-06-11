cdade3ac1b475a13fbc50f1221090560b7deb4c6
Vanity <ktkyletran@gmail.com>
2021-06-11 18:35:55 -0500

    added sticky footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container text-center">
+        <span>Copyright @ 2021</span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
