83942c69635482e7c1ea623dd3e5fa3d5e0a733f
Vanity <ktkyletran@gmail.com>
2021-06-11 18:40:00 -0500

    theme update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,47 +1,55 @@
 import React from 'react'
-import { Container, Row, Col, Card, Jumbotron } from 'react-bootstrap'
+import { Container, Row, Col, Jumbotron, Button } from 'react-bootstrap'
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 import { GrLocation } from 'react-icons/gr'
 import { BiPhone } from 'react-icons/bi'
 import { HiOutlineMail } from 'react-icons/hi'
 import myResume from '../resume/kyles-resume.pdf'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-      <Jumbotron className="text-center">
-        <h1>About Me</h1>
+    <div className="home-container">
+      <Jumbotron className="text-center" style={{ background: 'transparent', color: 'white', marginBottom: '0' }}>
+        <h1 style={{ fontSize: '50', textShadow: '2px 2px rgba(129, 135, 145)' }}>About Me</h1>
       </Jumbotron>
       <Container>
         <Row>
-          <Col style={{backgroundColor: 'blue'}}>
-            <img src="https://i.imgur.com/XQGK162m.jpg" alt="me" />
-            <p>Full Stack Web Developer with a B.S. in Biology and passionate in expanding current knowledge and skill sets. Effective at creating well-designed, user-friendly applications by combining creativity and conflict resolution. Recognized by peers for attention to detail, reliability, and being collaborative.</p>
+          <Col className="about-container" sm={12} md={6}>
+            <img src="https://imgur.com/XQGK162.jpg" alt="me" className="about-img"/>
           </Col>
-          <Col style={{backgroundColor: 'red'}}>
-            <Container style={{backgroundColor: 'green'}}>
-              <h1>Info</h1>
-              <p><span><GrLocation /></span> Austin, Texas</p>
-              <p><span><BiPhone /></span> 281-624-8908</p>
-              <p><span><HiOutlineMail /></span> ktkyletran@gmail.com</p>
-            </Container>
-            <Container>
-              <Row>
-                <Card Body style={{backgroundColor: 'yellow'}}>
-                  <a href="https://www.linkedin.com/in/kyle-tran-89245b1b9/" target="_blank" rel="noreferrer"><AiFillLinkedin /> LinkedIn</a>
-                </Card>
-              </Row>
-              <Row>
-                <Card Body style={{backgroundColor: 'yellow'}}>
-                  <a href="https://github.com/ktkyletran" target="_blank" rel="noreferrer"><AiFillGithub /> GitHub</a>
-                </Card>
-              </Row>
-              <Row>
-                <Card Body style={{backgroundColor: 'yellow'}}>
-                  <a download={'kyles-resume.pdf'} href={myResume}>Download Resume</a>
-                </Card>
-              </Row>
-            </Container>
+          <Col sm={12} md={6}>
+            <p className="about-desc">Full Stack Web Developer with a B.S. in Biology and passionate in expanding current knowledge and skill sets. Effective at creating well-designed, user-friendly applications by combining creativity and conflict resolution. Recognized by peers for attention to detail, reliability, and being collaborative.</p>
+            <br></br>
+          <Container className="info-container">
+            <Row>
+              <Col>
+                <h4 className="info-title">Info</h4>
+                <hr></hr>
+                <p><span><GrLocation /></span> Austin, Texas</p>
+                <p><span><BiPhone /></span> 281-624-8908</p>
+                <p><span><HiOutlineMail /></span> ktkyletran@gmail.com</p>
+              </Col>
+              <div className="split"></div>
+              <Col>
+              <h4 className="info-title">Social Media</h4>
+              <hr></hr>
+                <p>
+                  <a href="https://www.linkedin.com/in/kyle-tran-89245b1b9/" target="_blank" rel="noreferrer" className="social-link"><AiFillLinkedin /> LinkedIn</a>
+                </p>
+                <p>
+                  <a href="https://github.com/ktkyletran" target="_blank" rel="noreferrer" className="social-link"><AiFillGithub /> GitHub</a>
+                </p>
+              </Col>
+            </Row>
+          </Container>
+          <Container fluid="true" className="d-flex justify-content-center">
+            <Row>
+              <Col className="resume-container">
+                <a download={'kyles-resume.pdf'} href={myResume}><Button variant="light" size="lg" className="resume-btn">Download Resume</Button></a>
+              </Col>
+            </Row>
+          </Container>
           </Col>
         </Row>
       </Container>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,37 +1,62 @@
 import React from 'react'
 import ProjectCard from '../components/ProjectCard'
-import { Container, Row, Jumbotron } from 'react-bootstrap'
+import { Container, Row, Jumbotron, Col } from 'react-bootstrap'
+import './Portfolio.css'
 
 
 const Portfolio = () => {
   return (
-    <div>
-      <Jumbotron className="text-center">
-        <h1>Portfolio</h1>
+    <div className="portfolio-container">
+      <Jumbotron className="text-center" style={{ background: 'transparent', color: 'white', marginBottom: '0' }}>
+        <h1 style={{ fontSize: '50', textShadow: '2px 2px rgba(129, 135, 145)' }}>Portfolio</h1>
       </Jumbotron>
-      <Container >
-        <Row sm={1} md={2} lg={2}>
+      <Container className="project-container">
+        <Row sm={1} md={1} lg={2} style={{rowGap: '30px'}}>
+          <Col sm={12} md={6} lg={4}>
+            <ProjectCard 
+              src="https://imgur.com/tSmMLdg.jpg" 
+              title="Mind Managed" 
+              description="Tracks a user's daily mood, prescription use, and journal entry so that the user can keep a detailed record to show their doctor." 
+              deployedLink="https://obscure-beyond-60909.herokuapp.com/"
+              githubLink="https://github.com/ktkyletran/MindManaged/tree/main"
+            />
+          </Col>
+          <Col sm={12} md={6} lg={4}>
             <ProjectCard 
               src="https://i.imgur.com/SnrtCJI.png" 
               title="FoodFeed" 
               description="Allows users to search for a place to eat whether it's local or in another city and save their favorite location(s)." 
+              deployedLink="https://ktkyletran.github.io/FoodFeed/"
+              githubLink="https://github.com/ktkyletran/FoodFeed"
             />
+          </Col>
+          <Col sm={12} md={6} lg={4}>
             <ProjectCard 
               src="https://imgur.com/vUva716.png" 
               title="Weather App" 
-              description="Retrieves the current and 5-day forecast for any city." 
+              description="Retrieves the current and 5-day forecast for any city."
+              deployedLink="https://ktkyletran.github.io/weather-wizard/"
+              githubLink="https://github.com/ktkyletran/weather-wizard" 
             />
+          </Col>
+          <Col sm={12} md={6} lg={4}>
             <ProjectCard 
               src="https://imgur.com/26TgJbL.png" 
               title="Premiere Show" 
-              description="Premiere Show allows a user to create a screening discussion board to share their personal projects with friends." 
+              description="Premiere Show allows a user to create a screening discussion board to share their personal projects with friends."
+              deployedLink="https://hidden-reaches-62484.herokuapp.com/"
+              githubLink="https://github.com/ktkyletran/PremiereShow"  
             />
+          </Col>
+          <Col sm={12} md={6} lg={4}>
             <ProjectCard 
               src="https://imgur.com/ALThFYi.png"
               title="Technically Tech" 
               description="CMS-style blog site where aspiring, current, and former developers can discuss technical concepts, recent advancements, and new technologies."
+              deployedLink="https://technically-tech.herokuapp.com/login"
+              githubLink="https://github.com/ktkyletran/technically-tech"  
             />
-
+          </Col>
         </Row>
       </Container>
     </div>
